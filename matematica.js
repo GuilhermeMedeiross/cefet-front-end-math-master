4a9b11e6001bd567986616c2f5bb191d6cd48d49
Mr. Med <guilherme.lucas2807@gmail.com>
2019-04-24 19:03:35 -0300

    Corrigindo erros

--- matematica.js
+++ matematica.js
@@ -13,33 +13,28 @@ botaoResolverEl.addEventListener('click', function() {
     let verticeY = document.querySelector('#resultado-verticeY')
 
     if(a.value !== 0){
-        delta.value = Math.pow(b.value, 2) - 4*a.value*c.value
+        delta.value = Math.pow(b.value, 2) - (4*a.value*c.value)
 
         //Calculando as raízes
         if(delta.value > 0) {
             console.log("Δ > 0")
-            x1.value = (-b.value + Math.sqrt(delta.value) ) / 2*a.value
-            x2.value = (-b.value - Math.sqrt(delta.value) ) / 2*a.value
+            x1.value = (-b.value + Math.sqrt(delta.value) ) / (2*a.value)
+            x2.value = (-b.value - Math.sqrt(delta.value) ) / (2*a.value)
             qtdRaizes.value = '2 Diferentes'
-            verticeX.value = -b.value / 2*a.value
-            verticeY.value = -delta.value / 4*a.value
         }else if(delta.value == 0){
             console.log("Δ = 0")
-            x1.value = (-b.value + Math.sqrt(delta.value) ) / 2*a.value
-            x2.value = (-b.value - Math.sqrt(delta.value) ) / 2*a.value
+            x1.value = (-b.value + Math.sqrt(delta.value) ) / (2*a.value)
+            x2.value = (-b.value - Math.sqrt(delta.value) ) / (2*a.value)
             qtdRaizes.value = '2 Iguais'
-            verticeX.value = x1.value
-            verticeY.value = x2.value
         }else{
             console.log("Δ < 0")
             console.log(delta.value)
             qtdRaizes.value = 'Nenhuma'
             x1.value = ''
             x2.value = ''
-            verticeX.value = ''
-            verticeY.value = ''
         }
-
+        verticeX.value = -b.value / (2*a.value)
+        verticeY.value = -delta.value / (4*a.value)
         sentidoU.value = a.value > 0 ? 'CIMA' : 'BAIXO'
     }else{
         delta.value = ''
